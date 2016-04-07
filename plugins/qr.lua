a3ed96a31d33465109458ccfee5de89e436d982b
Pouya Poorrahman <poyaskysps@gmail.com>
2016-04-07 15:04:17 +0430

    Update qr.lua

--- plugins/qr.lua
+++ plugins/qr.lua
@@ -78,8 +78,8 @@ return {
       .."Or colors through decimals: (\"255-192-203\" is pink)"
   },
   patterns = {
-    '^qr "(%w+)" "(%w+)" (.+)$',
-    "^qr (.+)$"
+    '^!qr "(%w+)" "(%w+)" (.+)$',
+    "^!qr (.+)$"
   },
   run = run
 }
