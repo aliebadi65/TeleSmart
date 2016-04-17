ad97b6079920b866ee95ec766064e1abc8cd5cd4
Pouya Poorrahman <poyaskysps@gmail.com>
2016-04-17 22:19:27 +0430

    Update stats.lua

--- plugins/stats.lua
+++ plugins/stats.lua
@@ -91,10 +91,10 @@ local function bot_stats()
   return text
 end
 local function run(msg, matches)
-  if matches[1]:lower() == 'smart2' then -- Put everything you like :)
+  if matches[1]:lower() == 'smart' then -- Put everything you like :)
     local about = _config.about_text
     local name = user_print_name(msg.from)
-    savelog(msg.to.id, name.." ["..msg.from.id.."] used /smart2 ")
+    savelog(msg.to.id, name.." ["..msg.from.id.."] used /smart ")
     return about
   end 
   if matches[1]:lower() == "statslist" then
@@ -143,8 +143,8 @@ return {
     "^[#!/]([Ss]tats)$",
     "^[#!/]([Ss]tatslist)$",
     "^[#!/]([Ss]tats) (group) (%d+)",
-    "^[#!/]([Ss]tats) (smart2)",
-	"^[#!/]([Ss]mart2)"
+    "^[#!/]([Ss]tats) (smart)",
+	"^[#!/]([Ss]mart)"
     }, 
   run = run
 }
