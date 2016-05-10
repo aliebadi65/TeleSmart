b4dda19c0ae9880fc760031564b383951160dd5e
aliebadi65 <aliebadi66@gmail.com>
2016-05-10 16:09:39 +0430

    Update hi.lua

--- plugins/hi.lua
+++ plugins/hi.lua
@@ -5,18 +5,18 @@ local mina = 136141698
     local value = redis:hget(hash, msg.from.id)
      if not value then
         if msg.from.id == tonumber(mina) then 
-           text = text..'سلام بابایی\n\n'
+           text = text..'هوووووی چیکار به عشقم رقیه داری چشمات رو ببند برو گم شو عبضی \n\n'
          elseif is_admin2(msg.from.id) then
-           text = text..'سلام \n\n'
+           text = text..'رقیه \n\n'
          elseif is_owner2(msg.from.id, msg.to.id) then
-           text = text..'سلام \n\n'
+           text = text..'رقیه \n\n'
          elseif is_momod2(msg.from.id, msg.to.id) then
-           text = text..'سلام \n'
+           text = text..'رقیه \n'
      else
-           text = text..'سلام\n\n'
+           text = text..'رقیه\n\n'
       end
       else
-       text = text..'سلام '..value..'  \n\n'
+       text = text..'رقیه '..value..'  \n\n'
      end
 return text
     
@@ -27,8 +27,8 @@ return {
 
 
 "^[Ss]lm$",
-"^سلام$",
-"^salam",
+"^رقیه$",
+"^رقی",
 
   }, 
   run = run 
