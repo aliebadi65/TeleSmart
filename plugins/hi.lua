e56d0d4ccfbbcb8558abe6feee64ad51764ef4fd
aliebadi65 <aliebadi66@gmail.com>
2016-05-10 16:21:40 +0430

    Update hi.lua

--- plugins/hi.lua
+++ plugins/hi.lua
@@ -1,11 +1,11 @@
 local function run(msg, matches)
-local mina = 136141698
+local mina = 75662928
   local hash = 'rank:variables'
   local text = ''
     local value = redis:hget(hash, msg.from.id)
      if not value then
         if msg.from.id == tonumber(mina) then 
-           text = text..'هوووووی چیکار به عشقم رقیه داری چشمات رو ببند برو گم شو عبضی \n\n'
+           text = text..'بله مامان جوووونم فدات بشم\n'
          elseif is_admin2(msg.from.id) then
            text = text..'هوووووی چیکار به عشقم رقیه داری چشمات رو ببند برو گم شو عبضی \n \n\n'
          elseif is_owner2(msg.from.id, msg.to.id) then
