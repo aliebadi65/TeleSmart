7c771f52e93f2ee65794c9deb617e863e52a2209
aliebadi65 <aliebadi66@gmail.com>
2016-05-10 22:00:41 +0430

    Update support.lua

--- plugins/support.lua
+++ plugins/support.lua
@@ -6,32 +6,33 @@ local function callback(extra, success, result)
 end
 
 local function run(msg, matches)
- if matches[1] == 'sudo1' then
-        chat = 'chat#'..msg.to.id
-        user1 = 'user#'..136141698
-        chat_add_user(chat, user1, callback, false)
-	return "مدیر ربات با موفقیت وارد گروه شد :/"
-      end
-  if matches[1] == 'sudo2' then
-        chat = 'chat#'..msg.to.id
-        user2 = 'user#'..136141698
-        chat_add_user(chat, user2, callback, false)
-	return "مدیر دوم ربات با موفقیت وارد گروه شد :D"
-    end
-return {
-  description = "ساپورت تیم", 
-  usage = {
-    "/help دریافت دستورات", 
-	},
-  patterns = {
-    "^[!/.](sudo1)",
-    "^[!/.](sudo2)",
-    "^(sudo1)",
-    "^(sudo2)",
-    "^()",
-  }, 
-  run = run,
-}
+  local user = 136141698
 
+  if matches[1] == "پشتیبان" then
+    user = 'user#id'..136141698
+  end
+
+  -- The message must come from a chat group
+  if msg.to.type == 'chat' then
+    local chat = 'chat#id'..msg.to.id
+    chat_add_user(chat, user, callback, false)
+    return "سازنده ربات وارد گروه شد"
+  else 
+    return 'This isnt a chat group!'
+  end
 
 end
+
+return {
+  description = "پشتیبان", 
+  patterns = {
+    "^(پشتیبان)$"
+  }, 
+  run = run 
+}
+
+end
+--Copyright; @behroozyaghi
+--Persian Translate; @behroozyaghi
+--ch : @nod32team
+--کپی بدون ذکر منبع حرام است
